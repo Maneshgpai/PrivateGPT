2af71433cd8552624d6d0ba26a3bd694366e7e7e
unknown <akash.patil.git@gmail.com>
2023-07-09 16:47:11 +0530

    added api routes

---
+++ server/funcs.py
@@ -0,0 +1,69 @@
+import os
+import pickle
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, Document, ServiceContext, StorageContext, load_index_from_storage
+
+index = None
+stored_docs = {}
+index_name = "./saved_index"
+pkl_name = "stored_documents.pkl"
+
+def initialize_index():
+    """Create a new global index, or load one from the pre-set path."""
+    global index, stored_docs
+
+    service_context = ServiceContext.from_defaults(chunk_size_limit=512)
+    if os.path.exists(index_name):
+        index = load_index_from_storage(StorageContext.from_defaults(persist_dir=index_name),
+                                        service_context=service_context)
+    else:
+        index = GPTVectorStoreIndex([], service_context=service_context)
+        index.storage_context.persist(persist_dir=index_name)
+    if os.path.exists(pkl_name):
+        with open(pkl_name, "rb") as f:
+            stored_docs = pickle.load(f)
+
+def query_index(query_text):
+    """Query the global index."""
+    global index
+    response = index.as_query_engine().query(query_text)
+    return response
+
+def insert_into_index(doc_file_path, doc_id=None):
+    """Insert new document into global index."""
+    global index, stored_docs
+    document = SimpleDirectoryReader(input_files=[doc_file_path]).load_data()[0]
+    if doc_id is not None:
+        document.doc_id = doc_id
+
+    # Keep track of stored docs -- llama_index doesn't make this easy
+    stored_docs[document.doc_id] = document.text[0:200]  # only take the first 200 chars
+
+    index.insert(document)
+    index.storage_context.persist(persist_dir=index_name)
+
+    with open(pkl_name, "wb") as f:
+        pickle.dump(stored_docs, f)
+
+    return
+
+def get_documents_list():
+    """Get the list of currently stored documents."""
+    global stored_doc
+    documents_list = []
+    for doc_id, doc_text in stored_docs.items():
+        documents_list.append({"id": doc_id, "text": doc_text})
+
+    return documents_list
+
+# init the global index
+# if __name__ == "__main__":
+# print("Initializing index...")
+# initialize_index()
+# print("Initialised index")
+
+
---
+++ server/index.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
+import funcs
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api", methods=['GET'])
+def home():
+    return jsonify({"message":"Hello"})
+
+
+
+@app.route("/api/query", methods=["GET"])
+def query_index():
+    query_text = request.args.get("text", None)
+    if query_text is None:
+        return "No text found, please include a ?text=blah parameter in the URL", 400
+
+    response = funcs.query_index(query_text)
+    return str(response), 200
+
+
+@app.route("/api/uploadFile", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return "Please send a POST request with a file", 400
+    
+    filepath = None
+    try:
+        uploaded_file = request.files["file"]
+        filename = secure_filename(uploaded_file.filename)
+        filepath = os.path.join('documents', os.path.basename(filename))
+        uploaded_file.save(filepath)
+
+        if request.form.get("filename_as_doc_id", None) is not None:
+            funcs.insert_into_index(filepath, doc_id=filename)
+        else:
+            funcs.insert_into_index(filepath)
+    except Exception as e:
+        # cleanup temp file
+        if filepath is not None and os.path.exists(filepath):
+            os.remove(filepath)
+        return "Error: {}".format(str(e)), 500
+
+    # cleanup temp file
+    if filepath is not None and os.path.exists(filepath):
+        os.remove(filepath)
+
+    return "File inserted!", 200
+
+
+@app.route("/api/getDocuments", methods=["GET"])
+def get_documents():
+    document_list = funcs.get_documents_list_function()
+
+    return make_response(jsonify(document_list)), 200
+
+funcs.initialize_index()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
