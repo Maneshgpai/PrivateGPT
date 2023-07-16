47348ac0cb47fc7dd45717a7525e945a0a1ac54c
NormieNoob <63718854+NormieNoob@users.noreply.github.com>
2023-07-16 18:17:33 +0530

    created documents component

---
+++ client/pages/documents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Documents from './components/Documents'
+
+function documents() {
+  return (
+    <div>
+      <Navbar/>
+      <Documents/>
+    </div>
+  )
+}
+
+export default documents
