453a0677359015bf82b79f6dc810866e1d8398dd
NormieNoob <63718854+NormieNoob@users.noreply.github.com>
2023-08-02 15:49:40 +0530

    added new dropzone

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,11 @@
 import FileUpload from "./components/FileUpload"
 import Navbar from "./components/Navbar"
+import MyDropzone from "./components/MyDropone"
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <FileUpload/>
+      <MyDropzone className=" p-16 mt-10 border border-neutral-200 w-2/3 mx-auto"/>
     </div>
   )
 }
