25415b92f0d152f45fa8cc42e918363f04cddbf0
Maneshgpai <maneshgpai@gmail.com>
2023-09-13 17:43:39 +0530

    Fix for prod build
    Deleted files: client/Pages/components/Documents.2.jsx, client/Pages/components/documentsList.js, client/Pages/components/MyDropone.jsx
    
    Modified file:
    client/pages/components/Header.jsx

--- client/pages/components/Header.jsx
+++ client/pages/components/Header.jsx
@@ -16,16 +16,18 @@ export default function AppHeader() {
     >
       <div className="relative flex h-12 items-center justify-between">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 16 16"
-              fill="#858699"
-            >
-              <path d="M15 5.25A3.25 3.25 0 0 0 11.75 2h-7.5A3.25 3.25 0 0 0 1 5.25v5.5A3.25 3.25 0 0 0 4.25 14h7.5A3.25 3.25 0 0 0 15 10.75v-5.5Zm-3.5 7.25H7v-9h4.5a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2Zm-6 0H4.25a1.75 1.75 0 0 1-1.75-1.75v-5.5c0-.966.784-1.75 1.75-1.75H5.5v9Z"></path>
-            </svg>
-          </Disclosure.Button>
+          <Disclosure>
+            <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 16 16"
+                fill="#858699"
+              >
+                <path d="M15 5.25A3.25 3.25 0 0 0 11.75 2h-7.5A3.25 3.25 0 0 0 1 5.25v5.5A3.25 3.25 0 0 0 4.25 14h7.5A3.25 3.25 0 0 0 15 10.75v-5.5Zm-3.5 7.25H7v-9h4.5a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2Zm-6 0H4.25a1.75 1.75 0 0 1-1.75-1.75v-5.5c0-.966.784-1.75 1.75-1.75H5.5v9Z"></path>
+              </svg>
+            </Disclosure.Button>
+          </Disclosure>
         </div>
 
         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
