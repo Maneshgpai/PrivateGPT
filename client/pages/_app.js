3c078ef3d67b88ba40be251829dd14831dcd6ab1
Maneshgpai <maneshgpai@gmail.com>
2024-01-06 14:49:34 +0530

    Moved ref to STRIPE KEY from _app.js to env

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "@/styles/globals.css";
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe('pk_test_51ONGj9SGzDVqCKx1mc6lPSykMVRvItL9wvMvllR78JwP4xGTKmCbWTy5wzKfWBMPJqY6SgWReca1yqC1JhR0HeUD00j0mbKxbF');
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
 export default function App({ Component, pageProps }) {
   return (
