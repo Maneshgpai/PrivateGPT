0b0e06fba197b9cc934d309acfdce807c3dc2695
Maneshgpai <maneshgpai@gmail.com>
2024-01-15 00:20:44 +0530

    SVIX webhook protection, Capture Delete user, Capture Status_mod_dt
    SVIX webhook protection, Capture Delete user, Capture Status_mod_dt

--- server/index.py
+++ server/index.py
@@ -8,21 +8,17 @@ import openai
 from logging_config import logger
 from openai.error import AuthenticationError, APIError, RateLimitError, APIConnectionError, ServiceUnavailableError
 import traceback
-# import sentry_sdk
-# from sentry_sdk import capture_exception, capture_message
 import logging
 from dotenv import load_dotenv, find_dotenv
 import PyPDF2
-# from sentry_sdk.integrations.logging import LoggingIntegration
-# from sentry_sdk.integrations.flask import FlaskIntegration
 import json
 import base64
 from google.cloud import firestore
 import pytz
 import datetime
 import random
-import math
 import stripe
+from svix.webhooks import Webhook, WebhookVerificationError
 
 load_dotenv(find_dotenv())
 
@@ -61,52 +57,6 @@ CORS(app)
 
 @app.route("/api", methods=['GET'])
 
-####################################### START STRIPE ############################################
-# @app.route("/api/create-sub", methods=["POST"])
-# def createSub():
-#     try:
-#         user_id = request.args.get("uid")
-#         def get_cost_units(uid):
-#             # db = get_firebase_app()
-#             db = firestore.Client.from_service_account_info(firestore_key_json)
-#             collection_ref = db.collection(uid + "_usage")
-#             docs = collection_ref.stream()
-#             tot_units = 0
-#             for doc in docs:
-#                 data = doc.to_dict()
-#                 if "total_tokens" in data:
-#                     tot_units += data["total_tokens"]
-#             return tot_units
-#         tot_units = get_cost_units(user_id)
-#         # print(f"Total units consumed {tot_units}, by userid {user_id}")
-
-#         # METERED USAGE RECORDING GOES HERE        
-#         # https://stripe.com/docs/billing/subscriptions/usage-based/recording-usage
-#         # stripe.SubscriptionItem.create_usage_record('{{SUBSCRIPTION_ITEM_ID}}',quantity=tot_units,timestamp=<timestamp>,action='increment',)
-#         # TO REMOVE THIS LOGIC, AND CHANGE TO METERED USAGE LOGIC IN upload-file OR summarise-text API CALLS
-#         unit_amount = math.ceil(tot_units * 100)
-#         product = stripe.Product.create(name="privateGPT", description="private gpt")
-#         price = stripe.Price.create(
-#             unit_amount=unit_amount,
-#             currency="usd",
-#             recurring={"interval": "month", "usage_type": "metered"},
-#             product=product.id,
-#         )
-#         metered_price_id = price.id
-#         session = stripe.checkout.Session.create(
-#             shipping_address_collection={"allowed_countries": ["IN"]},
-#             line_items=[{"price": metered_price_id}],
-#             mode="subscription",
-#             success_url="http://localhost:3000" + "?success=true",
-#             cancel_url="http://localhost:3000" + "?canceled=true",
-#         )
-#     except Exception as error:
-#         # print(error)
-#         return jsonify(error=str(error)), 500
-#     # print(session.url)
-#     return jsonify({"checkout_url": session.url})  # redirect(session.url, code=303)
-####################################### END STRIPE ##############################################
-
 def home():
     return jsonify({"message": "Hello"})
 
@@ -140,9 +90,6 @@ def upload_files():
         
         uid = request.args.get('uid')
         userId = uid
-
-
-
         
         db = firestore.Client.from_service_account_info(firestore_key_json)
         user_ref = db.collection('users').document(userId)
@@ -200,13 +147,6 @@ def upload_files():
                             action='increment',
                         )
 
-
-############################# STRIPE Start ###################################
-                    # stripe.SubscriptionItem.create_usage_record(
-                    #     "si_IutmSSymhsWA5i",
-                    #     quantity=prompt_tokens+completion_tokens,
-                    #     timestamp=datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f'),)
-############################# STRIPE End ###################################
                     db.collection(uid+"_usage").document(str(datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')))\
                         .set({"timestamp": datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')\
                             ,"prompt_tokens":prompt_tokens,"completion_tokens":completion_tokens,"total_tokens":prompt_tokens+completion_tokens,"llm_cost":openai_funcs.getOpenaiApiCost(llmmodel,completion_tokens,prompt_tokens), "query_id":query_id})
@@ -479,9 +419,10 @@ def summarise_text():
 @app.route("/api/browser-extn", methods=["POST"])
 def browser_extn_search():
     try:
-        # print("Entered browser-extn *************")
         uid = request.args.get('uid')
-        if (uid == 'user_textUserBrowserExtn'):
+        if (uid == 'user_extnTestUser1'):
+            return jsonify({"summary": "User not valid"}), 401
+        elif (uid == 'user_textUserBrowserExtn'):
             firestore_key = str(os.environ['FIRESTORE_KEY'])[2:-1]
             firestore_key_json= json.loads(base64.b64decode(firestore_key).decode('utf-8'))
             db = firestore.Client.from_service_account_info(firestore_key_json)
@@ -499,41 +440,42 @@ def browser_extn_search():
                 db.collection(uid).document(str(datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')))\
                     .set({"timestamp": datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')\
                         ,"source":"browser_extn","query_id":query_id,"query":query_text,"error":error})
-                return jsonify({"message": error}), 401
+                return jsonify({"summary": error}), 401
             else:
                 llmmodel = os.environ['DEFAULT_LLM_MODEL']
                 openai.api_key = os.environ['OPENAI_API_KEY']
 
-                prompt = openai_funcs.setCodeGenPrompt(text.strip(), 'browser_extn')
+                prompt = openai_funcs.setCodeGenPrompt(text.strip(), 'browser_extn_medcode')
                 message = openai_funcs.setChatMsg('browser_extn', prompt)
                 prompt_tokens = openai_funcs.num_tokens_from_messages(message)
 
+            ######################## START ##############################
                 try:
                     response = openai_funcs.getResponse(False, llmmodel, message)
                 except AuthenticationError:
                     error = 'Incorrect API key provided. You can find your API key at https://platform.openai.com/account/api-keys.'
                     logger.error(error)
-                    return jsonify({"message": error}), 401
+                    return jsonify({"summary": error}), 401
                 except APIError:
                     error = 'Retry your request after a brief wait and contact us if the issue persists.'
                     logger.error(error)
-                    return jsonify({"message": error}), 401
+                    return jsonify({"summary": error}), 401
                 except RateLimitError:
                     error = 'The API key has reached the rate limit. Contact Admin if isue persists.'
                     logger.error(error)
-                    return jsonify({"message": error}), 401
+                    return jsonify({"summary": error}), 401
                 except APIConnectionError:
                     error = 'Check your network settings, proxy configuration, SSL certificates, or firewall rules.'
                     logger.error(error)
-                    return jsonify({"message": error}), 401
+                    return jsonify({"summary": error}), 401
                 except ServiceUnavailableError:
                     error = 'Retry your request after a brief wait and contact us if the issue persists. Check OpenAI status page: https://status.openai.com/'
                     logger.error(error)
-                    return jsonify({"message": error}), 401
+                    return jsonify({"summary": error}), 401
                 except:
                     error = "Error: {}".format(traceback.format_exc())
                     logger.error(error)
-                    return jsonify({"message": error}), 400
+                    return jsonify({"summary": error}), 400
 
                 resp = response['choices'][0]['message']['content']
                 completion_tokens = openai_funcs.num_tokens_from_response(resp)
@@ -548,38 +490,145 @@ def browser_extn_search():
                         ,"prompt_tokens":prompt_tokens,"completion_tokens":completion_tokens,"total_tokens":prompt_tokens+completion_tokens,"llm_cost":openai_funcs.getOpenaiApiCost(llmmodel,completion_tokens,prompt_tokens), "query_id":query_id})
 
                 return jsonify({"summary": resp}), 200
+            ####################### END ##########################
         else:
-            return jsonify({"message": "User not valid"}), 401
+            return jsonify({"summary": "User not valid"}), 401
+    except Exception as e:
+        error = "Error: {}".format(str(e))
+        logger.error(error)
+        return jsonify({"summary": error}), 400
+
+@app.route("/api/browser-extn-stream", methods=["POST"])
+def browser_extn_st_search():
+    try:
+        uid = request.args.get('uid')
+        if (uid == 'user_extnTestUser1'):
+            return jsonify({"summary": "User not valid"}), 401
+        elif (uid == 'user_textUserBrowserExtn'):
+            firestore_key = str(os.environ['FIRESTORE_KEY'])[2:-1]
+            firestore_key_json= json.loads(base64.b64decode(firestore_key).decode('utf-8'))
+            db = firestore.Client.from_service_account_info(firestore_key_json)
+            now = datetime.datetime.utcnow()
+            query_id = str(int((now.timestamp()*1000000))+random.randint(1000,9999))
+
+            data = request.get_json()
+            if 'text' not in data:
+                return jsonify({'error': 'Text not found in request'}), 400
+            text = data['text']
+            query_text = text.strip()
+            mod_response = openai.Moderation.create(input=text, )
+            if (mod_response['results'][0]['flagged']):
+                error = 'This text violates website\'s content policy! Please use content relevant to medical coding only.'
+                db.collection(uid).document(str(datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')))\
+                    .set({"timestamp": datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')\
+                        ,"source":"browser_extn","query_id":query_id,"query":query_text,"error":error})
+                return jsonify({"summary": error}), 401
+            else:
+                llmmodel = os.environ['DEFAULT_LLM_MODEL']
+                openai.api_key = os.environ['OPENAI_API_KEY']
+
+                prompt = openai_funcs.setCodeGenPrompt(text.strip(), 'browser_extn_medcode')
+                message = openai_funcs.setChatMsg('browser_extn', prompt)
+                prompt_tokens = openai_funcs.num_tokens_from_messages(message)
+
+            ######################## START ##############################
+                def generate():
+                    full_response = ""
+                    for resp in openai.ChatCompletion.create(model=llmmodel, messages=message, temperature=0, stream=True):
+                        if "content" in resp.choices[0].delta:
+                            text = resp.choices[0].delta.content
+                            final_text =text.replace('\n', '\\n')
+                            full_response += final_text
+                            yield f"{final_text}"
+                            # time.sleep(1)  # Simulating a delay
+                    db.collection(uid).document(str(datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')))\
+                        .set({"timestamp": datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')\
+                            ,"source":"paste_content","query_id":query_id,"query":query_text,"response":full_response.strip()})
+
+                    completion_tokens = openai_funcs.num_tokens_from_response(full_response.strip())
+
+                    db.collection(uid+"_usage").document(str(datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')))\
+                        .set({"timestamp": datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S.%f')\
+                            ,"prompt_tokens":prompt_tokens,"completion_tokens":completion_tokens,"total_tokens":prompt_tokens+completion_tokens,"llm_cost":openai_funcs.getOpenaiApiCost(llmmodel,completion_tokens,prompt_tokens), "query_id":query_id})
+                    # if user_data['stripe_subscription_id']:
+                    #     subscription_item_id = user_data['stripe_subscription_item_id']  # Retrieve the subscription item ID
+                        # total_tokens = prompt_tokens + completion_tokens
+                        # print("all tokens", total_tokens, subscription_item_id)
+                        # stripe.SubscriptionItem.create_usage_record(
+                        #     subscription_item_id,  # Use the retrieved subscription item ID
+                        #     quantity=total_tokens,
+                        #     api_key=stripe_secret_key,
+                        #     action='increment',
+                        # )
+
+                return Response(stream_with_context(generate()), content_type='text/event-stream')
+            ####################### END ##########################
+        else:
+            return jsonify({"summary": "User not valid"}), 401
+    except Exception as e:
+        error = "Error: {}".format(str(e))
+        logger.error(error)
+        return jsonify({"summary": error}), 400
+
+@app.route("/user-delete", methods=["POST"])
+def user_deleted():
+    # Authenticating Webhook from Clerk
+    webhook_secret = os.environ["USER_DELETE_WEBHOOK_SECRET"]
+    try:
+        headers = request.headers
+        payload = request.get_data()
+        wh = Webhook(webhook_secret)
+        msg = wh.verify(payload, headers)
+    except WebhookVerificationError as e:
+        return jsonify({"error": "Webhook not verified!"}), 400
+    try:
+        data = request.get_json()
+        uid = data['data']['id']
+        firestore_key = str(os.environ['FIRESTORE_KEY'])[2:-1]
+        firestore_key_json= json.loads(base64.b64decode(firestore_key).decode('utf-8'))
+        db = firestore.Client.from_service_account_info(firestore_key_json)
+        user_ref = db.collection('users').document(uid)
+        user_ref.update({'status': "clerk_account_deleted"})
+        user_ref.update({'status_upd_dt': firestore.SERVER_TIMESTAMP})
+        return jsonify({"message": "User account and associated data is deleted from server"}), 200
     except Exception as e:
         error = "Error: {}".format(str(e))
         logger.error(error)
         return jsonify({"message": error}), 400
 
+
 @app.route("/user-created", methods=["POST"])
 def user_created():
+
+    # Authenticating Webhook from Clerk
+    webhook_secret = os.environ["USER_CREATE_WEBHOOK_SECRET"]
+    
     try:
-        print("In user-created**********")
+        headers = request.headers
+        payload = request.get_data()
+        wh = Webhook(webhook_secret)
+        msg = wh.verify(payload, headers)
+    except WebhookVerificationError as e:
+        return jsonify({"error": "Webhook not verified!"}), 400
+
+    try:
         # Extract user data from the request
         data = request.get_json()
-        print("user-created> data:",data)
-        print("user-created> name:",data['data']['last_name'])
-        print("user-created> email:",data['data']['email_addresses'][0]['email_address'])
-
         # Create a Stripe customer
         stripe_customer = stripe.Customer.create(
             email=data['data']['email_addresses'][0]['email_address'],
             name=f"{data['data']['first_name']} {data['data']['last_name']}"
         )
-        print("Created stripe customer")
 
+        
         # Create a Stripe subscription
         stripe_std_coding_price = os.environ['STRIPE_STANDARD_CODING_PRICE_KEY']
         stripe_subscription = stripe.Subscription.create(
             customer=stripe_customer.id,
             items=[{'price': stripe_std_coding_price}]
         )
-        print("Created stripe subscription")
 
+        
         # Prepare user data for Firestore
         user_data = {
             'stripe_customer_id': stripe_customer.id,
@@ -591,24 +640,23 @@ def user_created():
             'last_name': data['data']['last_name'],
             'email': data['data']['email_addresses'][0]['email_address'],
             'created_at': firestore.SERVER_TIMESTAMP,
-            'status': 'signup'
-
+            'status': 'signup',
+            'status_upd_dt':firestore.SERVER_TIMESTAMP
         }
-        print("Prepared user data")
 
+        
         # Save to Firestore
         firestore_key = str(os.environ['FIRESTORE_KEY'])[2:-1]
         firestore_key_json= json.loads(base64.b64decode(firestore_key).decode('utf-8'))
         db = firestore.Client.from_service_account_info(firestore_key_json)
         db.collection('users').document(data['data']['id']).set(user_data)
-        print("Saved to user collection")
 
+        
         return jsonify({"message": "User created and Stripe subscription set"}), 200
     
     except Exception as e:
         error = "Error: {}".format(str(e))
         logger.error(error)
-        print("error:",error)
         return jsonify({"message": error}), 400
     
 
@@ -644,6 +692,7 @@ def update_user_status():
         db = firestore.Client.from_service_account_info(firestore_key_json)
         user_ref = db.collection('users').document(user_id)
         user_ref.update({'status': status})
+        user_ref.update({'status_upd_dt': firestore.SERVER_TIMESTAMP})
         return jsonify({"message": "User status updated successfully"})
     
     except Exception as e:
@@ -677,12 +726,10 @@ def get_user_data():
 @app.route('/api/add-payment-method', methods=['POST'])
 def add_payment_method():
     try:
-        print("In add-payment-method *************")
         data = request.json
         customer_id = data.get('customer_id')  # Replace with actual customer ID
         payment_method_id = data.get('payment_method_id')
         user_id = data.get('id')
-        print("customer_id:",customer_id,". payment_method_id:",payment_method_id,". user_id:",user_id)
         # Attach the payment method to the customer
         stripe.PaymentMethod.attach(
             payment_method_id,
@@ -702,16 +749,14 @@ def add_payment_method():
         db = firestore.Client.from_service_account_info(firestore_key_json)
         user_ref = db.collection('users').document(user_id)
         user_ref.update({'status': status})
-
-
-
+        user_ref.update({'status_upd_dt': firestore.SERVER_TIMESTAMP})
         return jsonify({'message': 'Payment method added successfully'}), 200
 
     except Exception as e:
-        print(traceback.format_exc())
+        # print(traceback.format_exc())
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) ### For Render
-    # app.run(debug=True, port=8080) ### For Local host
+    # app.run(host='0.0.0.0', port=5000, debug=False) ### For Render
+    app.run(debug=True, port=8080) ### For Local host
     # app.run(port=8080) ### For Local host
